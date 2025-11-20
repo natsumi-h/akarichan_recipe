f11bf1be08aa30992902fd1bc081331d4536b490
Natsumi.H <natsmy.1211@gmail.com>
2025-11-20 22:17:28 +0900

    update

---
+++ server/api/index.js
@@ -0,0 +1,2 @@
+// Vercel Serverless Function entry point
+export { default } from '../dist/server/index.js';
--- server/api/index.ts
+++
@@ -1,4 +0,0 @@
-// Vercel Serverless Function entry point
-import app from '../dist/server/index.js';
-
-export default app;
